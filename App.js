6897d8d10fa3d967934c95a3c6ff7c4b9f84a200
Chip Cerio <alistair.cerio@gmail.com>
2019-02-26 18:47:57 +0800

    Add prettier

--- App.js
+++ App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
-import Button from './src/components/common/Button';
-import Dialog from './src/components/common/Dialog';
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import Button from './src/components/common/Button'
+import Dialog from './src/components/common/Dialog'
 
 export default class App extends React.Component {
-
   state = {
     isModalVisible: false,
-  };
+  }
 
   toggleModal = () => {
-    this.setState({ isModalVisible: !this.state.isModalVisible });
+    this.setState({ isModalVisible: !this.state.isModalVisible })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Button title=" Toggle Modal "
-          onClick={this.toggleModal}
+        <Login />
+        <Button title=" Toggle Modal " onClick={this.toggleModal} />
+        <Dialog
+          isVisible={this.state.isModalVisible}
+          onBackdropPress={this.toggleModal}
         />
-        <Dialog isVisible={this.state.isModalVisible}
-        onBackdropPress={this.toggleModal}>
-
-        </Dialog>
       </View>
-    );
+    )
   }
 }
 
@@ -32,6 +30,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+    justifyContent: 'center',
+  },
+})
